1ada89427ca75433360de0bd5d8f35b4d6f85b76
Mohammed Raad <mohamedraad77@outlook.com>
2022-05-10 13:57:02 +0300

    CHANGES REQUESTED

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
-const navOpen = document.querySelector('.icon');
-const navClose = document.querySelector('.close-btn-wrapper');
-const menuElement = document.querySelector('.menu');
-const linksAction = menuElement.querySelectorAll('a');
+const navOpen = document.querySelector(".icon");
+const navClose = document.querySelector(".close-btn-wrapper");
+const menuElement = document.querySelector(".menu");
+const linksAction = menuElement.querySelectorAll("a");
 
-navOpen.addEventListener('click', () => {
-  menuElement.classList.remove('hidden');
+navOpen.addEventListener("click", function () {
+  menuElement.classList.remove("hidden");
+  document.querySelector("body").classList.add("overflow-hidden");
 });
 
-navClose.addEventListener('click', () => {
-  menuElement.classList.add('hidden');
+navClose.addEventListener("click", function () {
+  menuElement.classList.add("hidden");
+  document.querySelector("body").classList.remove("overflow-hidden");
 });
 
 linksAction.forEach((node) => {
-  node.onclick = () => menuElement.classList.add('hidden');
+  node.onclick = function () {
+    menuElement.classList.add("hidden");
+    document.querySelector("body").classList.remove("overflow-hidden");
+  };
 });
