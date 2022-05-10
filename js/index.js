a14cd6ac5da08232b1ed086df0d5f9227eb22ad1
unknown <sntakirutimana72@gmail.com>
2022-05-10 10:59:36 +0200

    Fix linting errors

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
-const navOpen = document.querySelector(".icon");
-const navClose = document.querySelector(".close-btn-wrapper");
-const menuElement = document.querySelector(".menu");
-const linksAction = menuElement.querySelectorAll("a");
+const navOpen = document.querySelector('.icon');
+const navClose = document.querySelector('.close-btn-wrapper');
+const menuElement = document.querySelector('.menu');
+const linksAction = menuElement.querySelectorAll('a');
 
-navOpen.addEventListener("click", function () {
-  menuElement.classList.remove("hidden");
+navOpen.addEventListener('click', () => {
+  menuElement.classList.remove('hidden');
 });
 
-navClose.addEventListener("click", function () {
-  menuElement.classList.add("hidden");
+navClose.addEventListener('click', () => {
+  menuElement.classList.add('hidden');
 });
 
 linksAction.forEach((node) => {
-  node.onclick = function () {
-    menuElement.classList.add("hidden");
-  };
+  node.onclick = () => menuElement.classList.add('hidden');
 });
